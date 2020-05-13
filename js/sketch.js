d418fbb945a66e47835421a4c66f69a119d69fd7
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-05-13 17:10:27 +0200

    base_crayon

--- js/sketch.js
+++ js/sketch.js
@@ -11,26 +11,23 @@ function setup() {
 function draw() {
     // Conditionellle nous permettant de savoir si la souris est appuyée
     // et si le timer est écoulé
-    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+    if (frameCount % (timer * 60) == 0) {
 
         pencil(windowWidth / 2, windowHeight / 2, random(100, 200), random(200, 400));
     }
 }
 
 function pencil(x, y, pencilWidth, pencilHeight) {
-    push(); // Start a new drawing state
     fill(0);
     noStroke();
     rect(x, y, pencilWidth, pencilHeight);
-    pop(); // Restore original state
+
+    triangle(x - pencilWidth / 2, y + pencilHeight / 2, x + pencilWidth / 2, y + pencilHeight / 2, x, y + 300);
 }
 
 
 
-function mousePressed() {
-    // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-    save();
-}
+
 
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
