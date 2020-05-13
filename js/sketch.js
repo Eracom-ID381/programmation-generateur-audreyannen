89c3cad272309d4c6ebbfabc3093f122c44200d7
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-05-13 16:51:06 +0200

    début_crayon

--- js/sketch.js
+++ js/sketch.js
@@ -4,34 +4,34 @@
 let timer = 1;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
+    createCanvas(window.innerWidth, window.innerHeight);
+    rectMode(CENTER);
 }
 
 function draw() {
-  // Conditionellle nous permettant de savoir si la souris est appuyée 
-  // et si le timer est écoulé
-  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    generator(width / 2, height / 2, height-150);
-  }
+    // Conditionellle nous permettant de savoir si la souris est appuyée
+    // et si le timer est écoulé
+    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+
+        pencil(windowWidth / 2, windowHeight / 2, random(100, 200), random(200, 400));
+    }
 }
 
-// Fonction avec quatres paramètres
-function generator(x, y, size) {
-  // fill utilise le paramètre de notre fonction
-  for (let offset = size; offset > 0; offset -= 10) { 
-      fill(random(0,230));
-      rect(x, y, offset, offset);
-      console.log(offset);
-  }
+function pencil(x, y, pencilWidth, pencilHeight) {
+    push(); // Start a new drawing state
+    fill(0);
+    noStroke();
+    rect(x, y, pencilWidth, pencilHeight);
+    pop(); // Restore original state
 }
 
-function mousePressed() { 
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-  save();
+
+
+function mousePressed() {
+    // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
+    save();
 }
 
-function windowResized() { 
+function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
