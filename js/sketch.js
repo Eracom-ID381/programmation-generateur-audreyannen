4c5f6f3688ad41be5dc1c8002a75dd594f3116ca
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-05-20 16:02:00 +0200

    prog orientée objet

--- js/sketch.js
+++ js/sketch.js
@@ -2,39 +2,50 @@
 
 // une unité = 1 seconde
 let timer = 1;
+let pencil;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     rectMode(CENTER);
+    pencil = new Pencil(windowWidth / 2, windowHeight / 2, random(100, 200), random(200, 400));
 }
 
 function draw() {
     // Conditionellle nous permettant de savoir si la souris est appuyée
     // et si le timer est écoulé
     if (frameCount % (timer * 60) == 0) {
-
-        pencil(windowWidth / 2, windowHeight / 2, random(100, 200), random(200, 400));
+        pencil.afficher();
     }
 }
 
-function pencil(x, y, pencilWidth, pencilHeight) {
-    background(0);
-    fill(random(200, 255), random(200, 250), random(0, 65));
-    noStroke();
-    rect(x, y, pencilWidth, pencilHeight);
+class Pencil {
+    constructor(x, y, pencilWidth, pencilHeight) {
+        this.x = x;
+        this.y = y;
+        this.pencilWidth = pencilWidth;
+        this.pencilHeight = pencilHeight;
+    }
 
-    triangle(x - pencilWidth / 2, y + pencilHeight / 2, x + pencilWidth / 2, y + pencilHeight / 2, x, y + pencilHeight);
+    afficher() {
+        background(0);
+        fill(random(200, 255), random(200, 250), random(0, 65));
+        noStroke();
+        rect(this.x, this.y, this.pencilWidth, this.pencilHeight);
 
-    stroke(80);
-    line(x + pencilWidth / 3, y + pencilHeight / 2, x + pencilWidth / 3, y - pencilHeight / 2);
-    line(x - pencilWidth / 3, y + pencilHeight / 2, x - pencilWidth / 3, y - pencilHeight / 2);
-    line(x - pencilWidth / 200, y + pencilHeight / 2, x - pencilWidth / 200, y - pencilHeight / 2);
+        triangle(this.x - this.pencilWidth / 2, this.y + this.pencilHeight / 2, this.x + this.pencilWidth / 2, this.y + this.pencilHeight / 2, this.x, this.y + this.pencilHeight);
 
-    noStroke();
-    fill(random(240, 255), random(150, 185), random(200, 215));
-    rect(x, y - pencilHeight / 2, pencilWidth, pencilHeight / 4);
-    fill(random(150, 200));
-    rect(x, y - pencilHeight / 3, pencilWidth, pencilHeight / 7);
+        stroke(80);
+        line(this.x + this.pencilWidth / 3, this.y + this.pencilHeight / 2, this.x + this.pencilWidth / 3, this.y - this.pencilHeight / 2);
+        line(this.x - this.pencilWidth / 3, this.y + this.pencilHeight / 2, this.x - this.pencilWidth / 3, this.y - this.pencilHeight / 2);
+        line(this.x - this.pencilWidth / 200, this.y + this.pencilHeight / 2, this.x - this.pencilWidth / 200, this.y - this.pencilHeight / 2);
+
+        noStroke();
+        fill(random(240, 255), random(150, 185), random(200, 215));
+        rect(this.x, this.y - this.pencilHeight / 2, this.pencilWidth, this.pencilHeight / 4);
+        fill(random(150, 200));
+        rect(this.x, this.y - this.pencilHeight / 3, this.pencilWidth, this.pencilHeight / 7);
+    }
+
 }
 
 
